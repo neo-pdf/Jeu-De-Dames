a1fcc4aed1a029ab2d19e6d84ef029c722d37257
sneo <neo.silvertand@ynov.com>
2025-01-14 13:41:05 +0100

    add README.md

--- Jeu.java
+++ Jeu.java
@@ -1,2 +1,7 @@
 public class Jeu {
+    String message = "Hello World!";
+    public static void main(String[] args) {
+        Jeu myObj = new Jeu();
+        System.out.println(myObj.message);
+    }
 }
