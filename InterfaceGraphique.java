3b7fa06dd66221e0306eb67a765d0959a46385a9
sneo <neo.silvertand@ynov.com>
2025-01-15 14:10:30 +0100

    création de l'interface graphique

--- InterfaceGraphique.java
+++ InterfaceGraphique.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class InterfaceGraphique {
+    public static void main(String[] args) {
+        createFrame frame = new createFrame();
+    }
 }
+class createFrame extends JFrame{
+    public createFrame() {
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setTitle("Jeu de dames");
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setUndecorated(false);
+        this.getContentPane().setBackground(new Color(153, 255, 204));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
+
--- Jeu.java
+++ Jeu.java
@@ -1,7 +1,10 @@
-public class Jeu {
-    String message = "Hello World!";
+import java.util.Scanner;  // Import the Scanner class
+
+class Jeu {
     public static void main(String[] args) {
-        Jeu myObj = new Jeu();
-        System.out.println(myObj.message);
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);  // Output user input
     }
 }
