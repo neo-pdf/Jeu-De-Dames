fb0d8745a1294c87395a1c46db5be703e574a733
Bimbam103 <bimbam2308@gmail.com>
2025-01-28 17:17:38 +0100

    ajout de la case

--- Case.java
+++ Case.java
@@ -1,4 +1,37 @@
+// Classe représentant une pièce
+class Piece {
+    private String type; // Exemple : "Blanc" ou "Noir"
+
+    public Piece(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
+
+// Classe représentant une case du plateau
 public class Case {
-    Piece piece = new Piece();
-    String name = piece.piece;
+    private Piece piece; // Peut être null si la case est vide
+
+    public Case() {
+        this.piece = null; // Par défaut, la case est vide
+    }
+
+    public void placerPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public void enleverPiece() {
+        this.piece = null;
+    }
+
+    public boolean estVide() {
+        return piece == null;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
 }
